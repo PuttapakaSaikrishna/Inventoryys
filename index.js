a96c4d77ac710a8413b4e69d58e1870955d17a1a
Puttapaka saikrishna <86375697+PuttapakaSaikrishna@users.noreply.github.com>
2024-11-25 17:39:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require("cors"); // importing cors middleware
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; // choose port from here like 8080, 3001
+
+// Use CORS middleware
+server.use(cors());  // This enables CORS for all routes
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
